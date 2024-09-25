fbfd0850486ae9efd2ca809d504a67f847169504
Eduardapac <eduardaleitepac@gmail.com>
2024-09-25 08:57:11 -0300

    25/09

---
+++ src/main/java/br/com/eduarda/orcamento/repositories/categoria/MunicipioRepositoryQuery.java
@@ -0,0 +1,10 @@
+package br.com.eduarda.orcamento.repositories.categoria;
+
+import br.com.eduarda.orcamento.model.Municipio;
+import br.com.eduarda.orcamento.repositories.filter.MunicipioFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface MunicipioRepositoryQuery {
+    public Page<Municipio> filtrar(MunicipioFilter municipioFilter, Pageable pageable);
+}
